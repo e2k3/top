220c8bb3bdf7c73b879edaf4645111769c3b95d7
e2k3 <43041483+e2k3@users.noreply.github.com>
2018-09-12 00:45:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "$"
+var prefix = ".";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
 
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : e2k`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : e2k ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Speical4Ever I .help `,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
-client.on('ready', () => {
-    client.user.setActivity("Top Gamers  | $help ",{type: ''})
 
-});
-  
   client.on('message',async message => {
   if(message.content.startsWith(prefix + "server")) {
     let embed = new Discord.RichEmbed()
